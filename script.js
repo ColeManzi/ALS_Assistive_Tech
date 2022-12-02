0e8f762d7b3268e75291d7b0a06bdd83b43e9526
Timothy Erckert <timothyerckert@gmail.com>
2022-12-02 00:25:24 -0500

    Initial changes

---
+++ script.js
@@ -0,0 +1,6 @@
+let submenu = document.getElementById('plug-submenu')
+let on = document.getElementById('plug-on')
+let off = document.getElementById('plug-off')
+let pcan = document.getElementById('plug-cancel')
+
+
