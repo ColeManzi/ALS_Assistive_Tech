907e95a02046330a1d955c2cadb094c5b3e1ba58
Tim <timothyerckert@gmail.com>
2022-12-05 19:11:15 -0500

    working on animation

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ let off = document.getElementById('plug-off')
 let pcan = document.getElementById('plug-cancel')
 
 
+const BGColors = {
+    'p1-emu':'red',
+    'p2-emu':'purple',
+    'p3-emu':'orange',
+    'p4-emu':'green'
+}
+
+
+const setOnOff = (gridID) => {
+    const ele = document.getElementById(gridID)
+    on.onclick =  () => ele.style.backgroundColor = BGColors[gridID]
+    off.onclick = () => ele.style.backgroundColor = 'black'
+    pcan.onclick = () => submenu.classList.add('hidden')
+    submenu.classList.remove('hidden')
+}
+
+
