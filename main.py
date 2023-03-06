e1bd48cd267baec1aa39be18b435896c027c82d4
Joseph Brown <jwbrown4@buffalo.edu>
2023-03-06 11:52:44 -0500

    eel link python end

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import eel
+import rfcontroller
 
+controller = rfcontroller.RFController() 
 
 @eel.expose
 def togglePlug(command):
-    print(command)
+    controller.sendcode(command)
 
 if __name__ == "__main__":
     eel.init('web')
-    eel.start('index.html')
+    eel.start('index.html', cmdline_args=['--start-fullscreen'])
