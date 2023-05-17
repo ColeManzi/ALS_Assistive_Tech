0c980ae154c57abd637d067750ddc7d7a31afc1a
Timothy Erckert <timothyerckert@gmail.com>
2023-05-17 08:24:38 -0400

    pushed absolute paths for autostartup

--- main.py
+++ main.py
@@ -16,15 +16,15 @@ def togglePlug(command):
 @eel.expose
 def storeConfig(setting, value):
     config = {}
-    with open('config.json', 'r') as openfile:
+    with open('/home/pi/450-mock-up/config.json', 'r') as openfile:
         config  = json.load(openfile)
-    with open('config.json', 'w') as writefile:
+    with open('/home/pi/450-mock-up/config.json', 'w') as writefile:
         config[setting] = value
         json.dump(config, writefile)
 
 @eel.expose
 def loadConfig():
-    with open('config.json', 'r') as openfile:
+    with open('/home/pi/450-mock-up/config.json', 'r') as openfile:
         config = json.load(openfile)
         eel.loadConfig(config)
 
@@ -33,7 +33,7 @@ def resetMouse():
     pyautogui.moveTo(0, screenHeight)
 
 if __name__ == "__main__":
-    eel.init('web', allowed_extensions=[".js",".html"])
+    eel.init('/home/pi/450-mock-up/web', allowed_extensions=[".js",".html"])
     resetMouse()
     eel.start('index.html', cmdline_args=['--start-fullscreen'])
     
