c7c59c497c81d4e1d1b71c4a725f963945ce7bc6
Cole <Colemanz@buffalo.edu>
2024-10-30 11:03:30 -0400

    Implemented Email sending feautre, will hold off on this till menu is set up

--- web/script.js
+++ web/script.js
@@ -79,8 +79,11 @@ ________________________________________________________________________________
 _____________________________________________________________________________________________________
 */
 const t2sContainer = document.getElementById("text-2-speech");
+//const t2sItems = t2sContainer.querySelectorAll(
+//  ".button-yes,.button-no,.button-starts-with,.button-ask-something,.button-large,.phrase-text,.button-TV-controls,.button-music,.button-outlet,.button-settings,.button-main-menu"
+//);
 const t2sItems = t2sContainer.querySelectorAll(
-  ".button-yes,.button-no,.button-starts-with,.button-ask-something,.button-large,.phrase-text,.button-TV-controls,.button-music,.button-outlet,.button-settings,.button-main-menu"
+  ".phrase-text,.button-main-menu, .button-send-email"
 );
 
 const keyboardContainer = document.getElementById("text-2-speech");
@@ -562,6 +565,38 @@ function speakPhrase() {
   textBox.innerText = "";
 }
 eel.expose(speakPhrase);
+
+/*
+--------------------------------------------------
+        SENDING EMAIL FUNCTION
+--------------------------------------------------
+*/
+
+
+function sendEmail() {
+    var textBox = document.getElementById("phrase-text-box");
+    var text = textBox.innerText; // Use .value for input box, .innerText or .textContent for div/span
+    const emailParams = {
+        subject: "None",
+        message: text
+    };
+
+    // Send the email using EmailJS
+    emailjs.send('service_c22kjag', 'template_hnlo6qs', emailParams)
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            //alert('Email sent successfully!');
+        }, function(error) {
+            console.error('FAILED...', error);
+            //alert('Failed to send email.');
+        });
+}
+
+
+
+
+
+
 /*
 --------------------------------------------------
 TV Control Functions
